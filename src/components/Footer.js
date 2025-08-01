6b265d09327eb92914da9cff2722f6d5f335b431
Eric Hess <110352704+EKHess@users.noreply.github.com>
2025-08-01 09:24:24 -0600

    Changed GitHub link to the repo for this project.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <div className='footer-container'>
             <p className='signature'>Made by <a href='#'>Eric Hess</a></p>
             <ul className='socials-container'>
-                <li><a href='https://github.com/EKHess' target='_blank'><ion-icon name="logo-github"></ion-icon></a></li>
+                <li><a href='https://github.com/EKHess/RAO-Skywatch' target='_blank'><ion-icon name="logo-github"></ion-icon></a></li>
                 <li><a href='https://x.com/EricHessOnline' target='_blank'><ion-icon name="logo-twitter"></ion-icon></a></li>
                 <li><a href='https://www.instagram.com/erichessonline/' target='_blank'><ion-icon name="logo-instagram"></ion-icon></a></li>
                 <li><a href='https://www.youtube.com/@erichessonline' target='_blank'><ion-icon name="logo-youtube"></ion-icon></a></li>
